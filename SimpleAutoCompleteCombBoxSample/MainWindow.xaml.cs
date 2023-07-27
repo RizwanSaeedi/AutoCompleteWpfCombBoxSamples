c4abdabb39ffb32bffcd962513452d4c2ea249ea
Rizwan Saeedi <mrizwansaeedi3@gmail.com>
2023-07-27 06:05:00 +0500

    .

--- SimpleAutoCompleteCombBoxSample/MainWindow.xaml.cs
+++ SimpleAutoCompleteCombBoxSample/MainWindow.xaml.cs
@@ -1,20 +1,7 @@
 ﻿using SaeediSoftWpfUiControls;
 
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SimpleAutoCompleteCombBoxSample
 {
@@ -27,12 +14,11 @@ namespace SimpleAutoCompleteCombBoxSample
         {
             InitializeComponent();
 
-
             ObservableCollection<SearchModel> dataList = new ObservableCollection<SearchModel>
             {
-                new SearchModel(){DisplayField = "Item 1",StringFeild1 = "Ali"},
-                new SearchModel(){DisplayField = "Item 2",StringFeild1 = "Frahan"},
-                new SearchModel(){DisplayField = "Item 3",StringFeild1 = "Raza"},
+                new SearchModel(){DisplayField = "Ali",StringFeild1 = "Ali"},
+                new SearchModel(){DisplayField = "Frahan",StringFeild1 = "Frahan"},
+                new SearchModel(){DisplayField = "Raza",StringFeild1 = "Raza"},
             };
 
             autoCompleteComboBox.ItemsSource = dataList;
